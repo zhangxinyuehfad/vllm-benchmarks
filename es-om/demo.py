52fda1872d227a09ced25d38d1949959300cbd9a
wangli <wangli858794774@gmail.com>
2025-02-25 14:13:48 +0800

    fix ci
    Signed-off-by: wangli <wangli858794774@gmail.com>

--- es-om/demo.py
+++ es-om/demo.py
@@ -1,9 +1,17 @@
 import os
 from pathlib import Path
 from typing import Union
+from handler import DataHandler
 
 
 def read_from_file(file_path: Union[str, Path]):
+    res = {}
     with open(file_path, 'r') as f:
-        data  = str.split(' ', 1)
-        commit_id, commit_title 
+        for line in f:
+            parts  = line.split(' ', 1)
+            commit_id, commit_title = parts[0], parts[1]
+            res[commit_id] = commit_title
+    return res
+
+data_handler = DataHandler()
+data_handler.index_name = 'vllm_benchmark_throuthput'
--- es-om/opt.py
+++ es-om/opt.py
@@ -2,7 +2,6 @@ from re import L
 from typing import Union
 from pathlib import Path
 from handler import DataHandler
-from common import VLLM_PULL_REQUEST_LIST
 
 from data import ServingDataEntry, LatencyDataEntry, ThroughputDataEntry, data_prc
 
@@ -10,7 +9,7 @@ from data import ServingDataEntry, LatencyDataEntry, ThroughputDataEntry, data_p
 data_handler = DataHandler()
 
 def fetch_pr_from_es(index_name: str = None):
-    sources = data_handler.search_data_from_vllm('vllm_benchmark_throughput', source=False)
+    sources = data_handler.search_data_from_vllm('vllm_benchmark_throughput', source=True)
     print(sources)
     return sources
 
@@ -19,15 +18,7 @@ def prc_json_to_es(folder_path: Union[str, Path]):
     return data_instance
 
 if __name__ == '__main__':
-    #res = prc_json_to_es('/Users/wangli/vllm-project/data/results')
-    # for k,v in res.items():
-    #     for data_entry in v:
-    #         commit_id = data_entry.commit_id + ('_' + str(data_entry.request_rate) if hasattr(data_entry, 'request_rate') else '')
-    #         data_handler.index_name = k
-    #         print(k)
-    #         print(commit_id)
-    #         print(data_entry.to_dict())
-    #         data_handler.add_single_data(commit_id, data_entry.to_dict())
+
     res = fetch_pr_from_es()
     for k,v in res.items():
         print(f"{k}:{v}")
