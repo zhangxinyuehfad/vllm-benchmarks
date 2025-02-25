e6182c0c3b984b6ae307fe5bfe3b42c945577746
wangli <wangli858794774@gmail.com>
2025-02-25 09:32:50 +0000

    fix
    Signed-off-by: wangli <wangli858794774@gmail.com>

--- es-om/send_to_es.py
+++ es-om/send_to_es.py
@@ -1,5 +1,9 @@
 import argparse
-from data import data_prc
+from tkinter import SE
+from typing import Union, Dict, List
+
+from handler import DataHandler
+from data import data_prc, ServingDataEntry, LatencyDataEntry, ThroughputDataEntry
 
 parser = argparse.ArgumentParser(description='add commit msg to es')
 
@@ -7,7 +11,17 @@ parser.add_argument('--commit_id', type=str, required=True)
 parser.add_argument('--commit_title', type=str, required=True)
 
 
+def send_data(data_instance: Dict[str, List[Union[ServingDataEntry, LatencyDataEntry, ThroughputDataEntry]]]):
+    datahandler =  DataHandler()
+    for index_name, data_list in data_instance.items():
+        datahandler.index_name = index_name
+        for data in data_list:
+            insert_id = "_".join([data.commit_id, str(data.request_rate)]) if hasattr(data, 'request_rate') else data.commit_id
+            datahandler.add_single_data(insert_id, data.to_dict())
+
 
 if __name__ == '__main__':
     args = parser.parse_args()
-    main(args.commit_id, args.commit_title)
+    data_instance = data_prc('/root/wl/oldfiles/vllm-project/vllm-benchmarks/benchmarks/results', args.commit_id, args.commit_title)
+    send_data(data_instance)
+    
