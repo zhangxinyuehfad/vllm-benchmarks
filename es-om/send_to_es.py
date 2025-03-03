456a8a93018d4ebf3526e1b4b1c9addc0b6b1c03
wangli <wangli858794774@gmail.com>
2025-03-03 09:49:36 +0800

    refactor send path
    Signed-off-by: wangli <wangli858794774@gmail.com>

--- es-om/send_to_es.py
+++ es-om/send_to_es.py
@@ -1,5 +1,6 @@
 import argparse
 from typing import Union, Dict, List
+import os
 
 from handler import DataHandler
 from data import data_prc, ServingDataEntry, LatencyDataEntry, ThroughputDataEntry
@@ -19,8 +20,16 @@ def send_data(data_instance: Dict[str, List[Union[ServingDataEntry, LatencyDataE
             datahandler.add_single_data(insert_id, data.to_dict())
 
 
+def get_abs_dir():
+    base_dir = os.path.dirname(__file__)
+    res_dir = os.path.join(base_dir, '../benchmarks/results')
+    res_dir = os.path.abspath(res_dir)
+    return res_dir
+
+
 if __name__ == '__main__':
     args = parser.parse_args()
-    data_instance = data_prc('../benchmarks/results', args.commit_id, args.commit_title)
-    send_data(data_instance)
+    data_instance = data_prc(get_abs_dir(), args.commit_id, args.commit_title)
+    # send_data(data_instance)
+    print(data_instance)
     
