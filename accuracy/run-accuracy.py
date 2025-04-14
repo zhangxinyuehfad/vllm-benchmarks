f09cb36eca3e96c1426b0215d71e9ba772ee897f
hfadzxy <59153331+hfadzxy@users.noreply.github.com>
2025-04-14 23:45:07 +0800

    Update run-accuracy.py

--- accuracy/run-accuracy.py
+++ accuracy/run-accuracy.py
@@ -27,7 +27,7 @@ import lm_eval
 import torch
 
 UNIMODAL_MODEL_NAME = ["Qwen/Qwen2.5-7B-Instruct", "meta-llama/Llama-3.1-8B-Instruct"]
-UNIMODAL_TASK = ["ceval-valid"]
+UNIMODAL_TASK = ["ceval-valid", "mmlu", "gsm8k"]
 # UNIMODAL_TASK = ["ceval-valid_computer_network", "ceval-valid_accountant"]
 MULTIMODAL_NAME = ["Qwen/Qwen2.5-VL-7B-Instruct"]
 MULTIMODAL_TASK = ["mmmu_val"]
