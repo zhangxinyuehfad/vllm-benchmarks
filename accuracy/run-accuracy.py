b64e2f6e80793b49f3a9b7f3f048001ad714ced9
hfadzxy <59153331+hfadzxy@users.noreply.github.com>
2025-04-15 08:33:47 +0800

    Update test_single_npu.yaml

--- accuracy/run-accuracy.py
+++ accuracy/run-accuracy.py
@@ -21,19 +21,19 @@ import gc
 import json
 import multiprocessing
 import sys
-import time
 
 import lm_eval
 import torch
 
 UNIMODAL_MODEL_NAME = ["Qwen/Qwen2.5-7B-Instruct", "meta-llama/Llama-3.1-8B-Instruct"]
 # UNIMODAL_TASK = ["ceval-valid", "mmlu", "gsm8k"]
-UNIMODAL_TASK = ["mmlu"]
+UNIMODAL_TASK = ["ceval-valid_accountant", "mmlu_formal_logic"]
 MULTIMODAL_NAME = ["Qwen/Qwen2.5-VL-7B-Instruct"]
-MULTIMODAL_TASK = ["mmmu_val"]
-#MULTIMODAL_TASK = ["mmmu_accounting"]
+# MULTIMODAL_TASK = ["mmmu_val"]
+MULTIMODAL_TASK = ["mmmu_accounting"]
 
-def run_accuracy_unimodal(queue, model=None, dataset=None):
+
+def run_accuracy_unimodal(queue, model, dataset):
     try:
         model_args = f"pretrained={model},max_model_len=4096,dtype=auto,tensor_parallel_size=2,gpu_memory_utilization=0.6"
         results = lm_eval.simple_evaluate(
@@ -51,12 +51,13 @@ def run_accuracy_unimodal(queue, model=None, dataset=None):
     except Exception as e:
         print(f"Error in run_accuracy_unimodal: {e}")
         queue.put(e)
-        sys.exit(1) 
+        sys.exit(1)
     finally:
         torch.npu.empty_cache()
         gc.collect()
 
-def run_accuracy_multimodal(queue, model=None, dataset=None):
+
+def run_accuracy_multimodal(queue, model, dataset):
     try:
         model_args = f"pretrained={model},max_model_len=8192,dtype=auto,tensor_parallel_size=2,max_images=2"
         results = lm_eval.simple_evaluate(
@@ -78,10 +79,29 @@ def run_accuracy_multimodal(queue, model=None, dataset=None):
         torch.npu.empty_cache()
         gc.collect()
 
-def generate_md(model_name, tasks_list):
+
+def generate_md(model_name, tasks_list, vllm_ascend_version, datasets):
+    file_name = str(model_name.split("/")[1]) + ".md"
+    preamble = f"""<div>
+    <strong>Software environment</strong>: vllm-ascend{vllm_ascend_version}.
+</div>
+<div>
+    <strong>Hardware environment</strong>: Atlas 800T A2 Series.
+</div>
+<div>
+    <strong>Datasets</strong>: {datasets}.
+</div>
+<div>
+    <strong>Run Command</strong>: python run-accuracy.py 
+        --model {model_name} 
+        --output {file_name}
+</div>
+<div>&nbsp;</div>
+"""
+
     header = (
-        "|                 Tasks                 |Version|Filter|n-shot|Metric|   |Value |   |Stderr|\n"
-        "|---------------------------------------|------:|------|-----:|------|---|-----:|---|-----:|"
+        "| Task                  | Version | Filter | n-shot | Metric   | Value   | Stderr |\n"
+        "|-----------------------|--------:|-------:|-------:|----------|--------:|-------:|"
     )
     rows = []
     for task_dict in tasks_list:
@@ -107,18 +127,26 @@ def generate_md(model_name, tasks_list):
             else:
                 n_shot = "0"
             row = (
-                f"| {task_name:<37} | {version:6d} | {flt:<6} | {n_shot:6} | {metric:<6} |↑  | {value:5.4f} |±  | {stderr:5.4f}|"
+                f"| {task_name:<37} "
+                f"| {version:>6d} "
+                f"| {flt:<6} "
+                f"| {n_shot:6} "
+                f"| {metric:<6} "
+                f"| ↑ {value:>5.4f} "
+                f"| ± {stderr:>5.4f} |"
             )
             rows.append(row)
-    md = header + "\n" + "\n".join(rows)
+    md = preamble + "\n" + header + "\n" + "\n".join(rows)
     print(md)
     return md
 
-def safe_md(args, accuracy):
-    print("accaurcy:", accuracy)
+
+def safe_md(args, accuracy, datasets):
     data = json.loads(json.dumps(accuracy))
     for model_key, tasks_list in data.items():
-        md_content = generate_md(model_key, tasks_list)
+        md_content = generate_md(
+            model_key, tasks_list, args.vllm_ascend_version, datasets
+        )
         with open(args.output, "w", encoding="utf-8") as f:
             f.write(md_content)
         print(f"create Markdown file:{args.output}")
@@ -129,27 +157,35 @@ def main(args):
     accuracy[args.model] = []
     result_queue: Queue[float] = multiprocessing.Queue()
     if args.model in UNIMODAL_MODEL_NAME:
+        datasets = ",".join(UNIMODAL_TASK)
         for dataset in UNIMODAL_TASK:
-            p = multiprocessing.Process(target=run_accuracy_unimodal, args=(result_queue, args.model, dataset))
+            p = multiprocessing.Process(
+                target=run_accuracy_unimodal, args=(result_queue, args.model, dataset)
+            )
             p.start()
             p.join()
             result = result_queue.get()
             print(result)
             accuracy[args.model].append(result)
     if args.model in MULTIMODAL_NAME:
+        datasets = ",".join(MULTIMODAL_TASK)
         for dataset in MULTIMODAL_TASK:
-            p = multiprocessing.Process(target=run_accuracy_multimodal, args=(result_queue, args.model, dataset))
+            p = multiprocessing.Process(
+                target=run_accuracy_multimodal, args=(result_queue, args.model, dataset)
+            )
             p.start()
             p.join()
             result = result_queue.get()
             print(result)
             accuracy[args.model].append(result)
     print(accuracy)
-    safe_md(args, accuracy)
-     
+    safe_md(args, accuracy, datasets)
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--output', type=str, required=True)
-    parser.add_argument('--model', type=str, required=True)
+    parser.add_argument("--output", type=str, required=True)
+    parser.add_argument("--model", type=str, required=True)
+    parser.add_argument("--vllm_ascend_version", type=str, required=True)
     args = parser.parse_args()
     main(args)
