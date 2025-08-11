3b486788fbd24bbd85c2c3862a1df877d40af22e
wangli <wangli858794774@gmail.com>
2025-08-11 17:43:01 +0800

    test
    Signed-off-by: wangli <wangli858794774@gmail.com>

--- .github/workflows/scripts/docker_exec.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-ENV_FILE="/root/actions-runner/.cache/set_env.sh"
-
-echo "GITHUB_WORKSPACE=${GITHUB_WORKSPACE}"
-docker exec \
-  ascend_ci_a3 \
-  /bin/bash -c "source \"$ENV_FILE\" && export PYTHONPATH=\"${GITHUB_WORKSPACE//\\/\/}/python:\${PYTHONPATH:-}\" && exec \"\$@\"" \
-  bash "$@"
